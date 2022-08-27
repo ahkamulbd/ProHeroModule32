6c3137fcd60b000602d235be0ec242e4493f1d06
ahkamulbd <ahkamulbd@gmail.com>
2022-08-27 14:53:34 +0600

    Rest of Lecture 31.5 done

--- json.js
+++ json.js
@@ -1,7 +1,31 @@
 // JSON - JavaScript Object Notation
 
+// Example 1:
+
 const user = { id: 1, name: 'Gorib Amir', job: 'actor' };
 const stringified = JSON.stringify(user);
 
 console.log(user);
 console.log(stringified);
+
+// Example 2:
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'haspatal road',
+        city: 'chakaria',
+        country: 'BD'
+    },
+    products: ['laptop', 'microphone', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+}
+console.log(shop);
+
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+
+const shopObj = JSON.parse(shopJSON);
+console.log(shopJSON);
